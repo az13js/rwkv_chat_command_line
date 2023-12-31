62ee20e440881efddd9a644f5f5f0727d450e0e3
庵中十三居士 <1654602334@qq.com>
2023-12-31 14:31:44 +0000

    有两个类依赖langchain相关的包，注释掉它们不影响使用
    Signed-off-by: 庵中十三居士 <1654602334@qq.com>

--- chat_models/__init__.py
+++ chat_models/__init__.py
@@ -1,9 +1,10 @@
-from .RWKVChat import RWKVChat
-from .RWKVLLM import RWKVLLM
+# 需要langchain，但是我开源的聊天脚本是用不到langchain的，所以我提交个commit给注释掉。
+#from .RWKVChat import RWKVChat
+#from .RWKVLLM import RWKVLLM
 from .Mirostat import Mirostat
 
 __all__ = [
-    'RWKVChat',
-    'RWKVLLM',
+    #'RWKVChat',
+    #'RWKVLLM',
     'Mirostat'
 ]
